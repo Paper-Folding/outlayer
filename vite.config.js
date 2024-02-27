20565116f04978870888c9c725a783ef5b556661
chenjianbo <specialwave@foxmail.com>
2024-02-27 09:51:37 +0800

    fix case

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     build: {
         lib: {
             entry: resolve(__dirname, "src/lib/outlayer.js"),
-            name: "outlayer",
+            name: "Outlayer",
             fileName: "outlayer",
             formats: ["umd"]
         },
